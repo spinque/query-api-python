5d09672fc6d4a7df941c1fb816e840f8a7caa811
Chris Kamphuis <mail@chriskamphuis.com>
2023-07-13 11:18:28 +0200

    Updated email in setup.py
    Also remove unnecessary import

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='spinque-query-api',
     version='0.0.1',
     description='Library to use the Spinque Query API in your Python project.',
     author='Spinque BV',
-    author_email='chris@cs.ru.nl',
+    author_email='chris@spinque.com',
     url='https://github.com/spinque/query-api-python/',
     license='MIT License'
 )
