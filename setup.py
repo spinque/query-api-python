8a9b17ed5c8ac56f053d6e5f85d63e53cdf4eaa0
Chris Kamphuis <mail@chriskamphuis.com>
2023-07-13 11:17:25 +0200

    Add setup.py for installations

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spinque-query-api',
+    version='0.0.1',
+    description='Library to use the Spinque Query API in your Python project.',
+    author='Spinque BV',
+    author_email='chris@cs.ru.nl',
+    url='https://github.com/spinque/query-api-python/',
+    license='MIT License'
+)
