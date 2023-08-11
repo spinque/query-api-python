a5dac44aa09ee2ed2af9492eb336d75871e11904
Chris Kamphuis <mail@chriskamphuis.com>
2023-08-11 15:35:56 +0200

    Add requests as a dependency
    - Add it in setup.py
    - Add a requirements.txt with requests

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description='Library to use the Spinque Query API in your Python project.',
     author='Spinque BV',
     author_email='chris@spinque.com',
+    install_requires=['requests'],
     url='https://github.com/spinque/query-api-python/',
     license='MIT License'
 )
