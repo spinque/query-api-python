9d20c40c2554980fc4362b40b76b145662689c5b
Chris <chris@spinque.com>
2024-06-12 14:53:50 +0200

    Fix type fetch function

--- spinque-query-api/api.py
+++ spinque-query-api/api.py
@@ -24,7 +24,7 @@ class Api:
               queries: Union[Query, List[Query]],
               options=None,
               result_type: RequestType = RequestType.RESULTS
-              ) -> str:
+              ) -> dict:
         if options is None:
             options = dict()
         headers = {}
@@ -37,7 +37,7 @@ class Api:
         return response_handler(r)
 
 
-def response_handler(response: requests.models.Response) -> str:
+def response_handler(response: requests.models.Response) -> dict:
     match response.status_code:
         case 200:
             return response.json()
